965c17bc7dd9bfe6fcfa5e91a6e639c2b07cf841
alessio.menchetti <alessio.menchetti@stud.unifi.it>
2021-04-13 03:43:11 -0700

    Rilascio v1.0

---
+++ version.h
@@ -0,0 +1,10 @@
+//
+// Created by lullibeps on 13/04/21.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+const std::string VERSION = "1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
